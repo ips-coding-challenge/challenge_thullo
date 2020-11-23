627245bf454e08ebeafd6ca92fd1b8ccdd988802
ips-coding-challenge <coding_challenge@laposte.net>
2020-11-23 12:06:52 +0100

    Feat: Add invitation feature (#19)
    * Start working on invitations for board
    
    * Feat: Add the invitation workflow

---
+++ src/routes/invitation.routes.ts
@@ -0,0 +1,9 @@
+import Router from '@koa/router'
+import InvitationController from '../controllers/InvitationController'
+import jwt from '../middlewares/jwt'
+const router = new Router()
+
+router.get('/invitations/:token', jwt, InvitationController.validToken)
+router.post('/invitations', jwt, InvitationController.store)
+
+export default router
