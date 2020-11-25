a262d352dd8f24183d756afbc8fa4bd0f9a092ec
ips-coding-challenge <coding_challenge@laposte.net>
2020-11-25 14:03:50 +0100

    Feat: Add the route to fetch a task (#29)

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -4,6 +4,7 @@ import jwt from '../middlewares/jwt'
 const router = new Router()
 
 router.get('/tasks', jwt, TaskController.index)
+router.get('/tasks/:id', jwt, TaskController.show)
 router.post('/tasks', jwt, TaskController.store)
 router.put('/tasks/:id', jwt, TaskController.update)
 
