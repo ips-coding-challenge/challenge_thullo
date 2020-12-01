2550d92e8bb063aec7de0e515fd5545fbdae3281
ips-coding-challenge <coding_challenge@laposte.net>
2020-12-01 18:07:55 +0100

    Update the knexfile.ts for production

--- knexfile.ts
+++ knexfile.ts
@@ -33,6 +33,10 @@ module.exports = {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      ssl: {
+        ssl: true,
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 2,
