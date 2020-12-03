f8a4cc8aa4dee570ffb8fdc69b35f15b750ffbb1
ips-coding-challenge <coding_challenge@laposte.net>
2020-12-03 20:29:28 +0100

    Add a test to verify that I update only the connected user

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 import { knex, server, chai } from './setup'
 import { createBoard, createList, createUser, generateJwt } from './utils'
 
@@ -36,6 +37,26 @@ describe('Users', () => {
     res.status.should.equal(422)
     console.log(res.text)
     // res.body.data.avatar.should.equal('https://newAvatar.com')
+  })
+
+  it('should only update one user', async () => {
+    const user = await createUser()
+    const secondUser = await createUser('second', 'second@test.fr')
+
+    const res = await chai
+      .request(server)
+      .put('/api/users')
+      .send({
+        avatar: 'http://test.jpg',
+      })
+      .set('Authorization', 'Bearer ' + (await generateJwt(user)))
+
+    res.status.should.equal(201)
+
+    const [userUpdated] = await knex('users').where('id', user.id)
+    const [secondUserTest] = await knex('users').where('id', secondUser.id)
+    userUpdated.avatar.should.equal('http://test.jpg')
+    expect(secondUserTest.avatar).to.be.null
   })
 
   afterEach(() => {
